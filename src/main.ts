5837a2f7037b9bb76b15b393f14cea29c21187c4
Paul Jensen <paulbjensen@gmail.com>
2025-04-07 17:17:06 +0100

    Added Sentry

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+import * as Sentry from "@sentry/svelte";
+
+// Initialize Sentry
+const sentryDSN = "https://fc0c7f164bdc3e05e84de2ae8efc439d@o95413.ingest.us.sentry.io/4509112877252608";
+Sentry.init({dsn: sentryDSN });
+
 const app = mount(App, {
   target: document.getElementById('app')!,
 })
