158cc8f40c4dd57fe1bcab578b47e7ccc7795e5d
Paul Jensen <paulbjensen@gmail.com>
2025-04-07 17:21:23 +0100

    Added Sentry sourcemaps upload for helping with error stacktraces

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), sentryVitePlugin({
+    org: "anephenix",
+    project: "lets-make-sweet-music"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
