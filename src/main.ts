4093c5a04d20e26089132e723bcc6aa7b935e3cd
Paul Jensen <paulbjensen@gmail.com>
2025-04-09 14:04:46 +0100

    Set the environment for Sentry to pick up when reporting errors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import "./app.css";
 import App from "./App.svelte";
 
 import * as Sentry from "@sentry/svelte";
+const environment = import.meta.env.VITE_ENV;
 
 // Initialize Sentry
 const sentryDSN =
 	"https://fc0c7f164bdc3e05e84de2ae8efc439d@o95413.ingest.us.sentry.io/4509112877252608";
-Sentry.init({ dsn: sentryDSN });
+Sentry.init({ dsn: sentryDSN, environment });
 
 const appElement: HTMLElement | null = document.getElementById("app");
 
